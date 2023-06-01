d3b8204f12b749fe2e5a143b56d20019dc0fc030
zlept55 <rochinhas.pais@gmail.com>
2023-06-01 17:49:09 +0000

    first commit 1/06/23

---
+++ taw-2023/src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button v-on:click="OnClick()" :style="{background: color  }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    name:'Button',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods:{
+        OnClick(){
+           this.$emit('btn-click')     
+        },
+    },
+}
+</script>
